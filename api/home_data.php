f1dc206882cd09c9dcf42fb9d2ddfcbd40da92ba
Shiv Singh <shivsingh7150@hotmail.com>
2024-05-17 16:36:20 +0530

    Update home_data.php

--- api/home_data.php
+++ api/home_data.php
@@ -7,7 +7,7 @@ $lats = $data['lats'];
 $longs = $data['longs'];
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(0);
 
 function calculateDistance($originLat, $originLng, $destLat, $destLng, $apiKey) {
     $unit = "K";
