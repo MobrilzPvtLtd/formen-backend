7e40c6489ab1da1f189a70ce52757253c49694b3
Shiv Singh <shiv@srapsware.com>
2024-08-13 18:31:02 +0530

    Revert "Refactor edit_message.php for updating chat messages"
    This reverts commit 59ff34c0768e1959b6d34c0ff0ff177b18e409cb.

--- api/chat/chat2.php
+++ api/chat/chat2.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Live Chat</title>
-    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
     <div id="chat-container">
         <div id="chat-messages">
-            </div>
+        </div>
     </div>
 
     <div id="input-area">
@@ -18,10 +20,10 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        $(document).ready(function() {
-            // Replace with actual user IDs 
-            var senderId = 2; 
-            var receiverId = 1; 
+        $(document).ready(function () {
+            // Replace with actual user IDs (you'll need to implement user authentication)
+            var senderId = 2;
+            var receiverId = 1;
 
             // Function to send a message via AJAX
             function sendMessage() {
@@ -31,21 +33,21 @@
                 $.ajax({
                     type: 'POST',
                     url: 'send_message.php',
-                    data: { 
-                        sender_id: senderId, 
-                        receiver_id: receiverId, 
-                        message: message 
+                    data: {
+                        sender_id: senderId,
+                        receiver_id: receiverId,
+                        message: message
                     },
-                    success: function(response) {
+                    success: function (response) {
                         if (response.status === 'success') {
                             $('#message-input').val('');
-                            displayMessage(message, true); 
-                            getMessages(); 
+                            displayMessage(message, true);
+                            getMessages();
                         } else {
-                            alert(response.message); 
+                            alert(response.message);
                         }
                     },
-                    error: function() {
+                    error: function () {
                         alert('Error sending message. Please try again.');
                     }
                 });
@@ -56,11 +58,11 @@
                 $.ajax({
                     type: 'GET',
                     url: 'get_messages.php',
-                    data: { 
-                        sender_id: senderId, 
-                        receiver_id: receiverId 
+                    data: {
+                        sender_id: senderId,
+                        receiver_id: receiverId
                     },
-                    success: function(response) {
+                    success: function (response) {
                         if (response.status === 'success') {
                             var messages = response.messages;
                             $('#chat-messages').empty();
@@ -68,8 +70,7 @@
                             for (var i = 0; i < messages.length; i++) {
                                 var message = messages[i].message;
                                 var isSent = (messages[i].sender_id == senderId);
-                                var messageId = messages[i].id; 
-                                displayMessage(message, isSent, messageId);
+                                displayMessage(message, isSent);
                             }
 
                             $('#chat-container').scrollTop($('#chat-container')[0].scrollHeight);
@@ -77,7 +78,7 @@
                             // Handle error
                         }
                     },
-                    error: function() {
+                    error: function () {
                         // Handle error
                     }
                 });
@@ -86,60 +87,19 @@
             // Function to determine if the response contains only new messages
             function isNewMessageResponse(response) {
                 // Example: If your PHP script wraps new messages in a specific container
-                return $(response).find('.new-message-container').length > 0; 
+                return $(response).find('.new-message-container').length > 0;
             }
 
-            // Function to display a new message in the chat (modified)
-            function displayMessage(message, isSent, messageId) { 
+            // Function to display a new message in the chat
+            function displayMessage(message, isSent) {
                 var messageClass = isSent ? 'sent' : 'received';
-                var newMessage = $('<div class="message ' + messageClass + '" data-message-id="' + messageId + '">' + message);
-
-                if (isSent) {
-                    var editButton = $('<button class="edit-button">Edit</button>');
-                    newMessage.append(editButton); 
-
-                    editButton.click(function() {
-                        var messageDiv = $(this).parent();
-                        var originalMessage = messageDiv.text().trim(); 
-
-                        var editInput = $('<input type="text" value="' + originalMessage + '">');
-                        var saveButton = $('<button class="save-button">Save</button>');
-
-                        messageDiv.empty().append(editInput).append(saveButton);
-
-                        saveButton.click(function() {
-                            var newMessageText = editInput.val();
-                            var messageId = messageDiv.data('message-id');
-
-                            $.ajax({
-                                type: 'POST',
-                                url: 'edit_message.php', 
-                                data: { 
-                                    message_id: messageId,
-                                    new_message: newMessageText
-                                },
-                                success: function(response) {
-                                    if (response.status === 'success') {
-                                        messageDiv.empty().text(newMessageText).append(editButton); 
-                                        getMessages(); 
-                                    } else {
-                                        alert(response.message); 
-                                    }
-                                },
-                                error: function() {
-                                    alert('Error editing message. Please try again.');
-                                }
-                            });
-                        });
-                    });
-                }
-
+                var newMessage = $('<div class="message ' + messageClass + '">' + message + '</div>');
                 $('#chat-messages').append(newMessage);
             }
 
             // Send message on button click or Enter key press
             $('#send-button').click(sendMessage);
-            $('#message-input').keypress(function(e) {
+            $('#message-input').keypress(function (e) {
                 if (e.which === 13) {
                     sendMessage();
                 }
@@ -149,9 +109,10 @@
             getMessages();
 
             // Poll for new messages every few seconds
-            setInterval(getMessages, 3000); 
+            setInterval(getMessages, 3000);
         });
     </script>
 
 </body>
+
 </html>
