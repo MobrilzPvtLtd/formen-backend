5abe1980fe06a6104641c51f8c80e26045934392
Shiv Singh <shiv@srapsware.com>
2024-08-14 15:49:08 +0530

    Refactor user_last_available.php to update the last available time for a user

---
+++ api/user_last_available.php
@@ -0,0 +1,23 @@
+<?php
+require dirname(dirname(__FILE__)) . '/inc/Connection.php';
+require dirname(dirname(__FILE__)) . '/inc/Gomeet.php';
+$data = json_decode(file_get_contents('php://input'), true);
+header('Content-type: text/json');
+$uid = $data['uid'];
+
+// Choose which variable to return based on your preference
+$result = $first;
+
+
+if ($uid == '') {
+    $returnArr = array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Something Went wrong  try again !");
+} else {
+    $CurrentTime = date("Y-m-d H:i:s");
+
+    $check = $dating->query("UPDATE `tbl_user` SET `last_available` = $CurrentTime WHERE `tbl_user`.`id` = $uid");
+
+    $returnArr = array("ResponseCode" => "200", "Result" => "true", "ResponseMsg" => "Last Available Updated Successfully!!!");
+
+}
+echo json_encode($returnArr);
+?>
